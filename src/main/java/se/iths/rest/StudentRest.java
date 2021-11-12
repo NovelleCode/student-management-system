0ee814796eb1a4901fc73d3a8137a571e8c22991
Karen Batjes <batjeskaren@gmail.com>
2021-11-12 13:45:56 +0100

    Changed GET annotation to POST

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -5,7 +5,7 @@ import se.iths.service.StudentService;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
@@ -21,7 +21,7 @@ public class StudentRest {
     StudentService studentService;
 
     @Path("")
-    @GET
+    @POST
     public Response createStudent(Student student) {
         studentService.createStudent(student);
         return Response.ok(student).build();
