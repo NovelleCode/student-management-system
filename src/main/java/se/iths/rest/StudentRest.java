b5bfb5a6315c2e3beb55ad4e7334439eebe7846c
Karen Batjes <batjeskaren@gmail.com>
2021-11-12 14:14:34 +0100

    Added endpoint and service method (with basic error handling) for retrieving all students

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -4,11 +4,9 @@ import se.iths.entity.Student;
 import se.iths.service.StudentService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -25,5 +23,12 @@ public class StudentRest {
     public Response createStudent(Student student) {
         studentService.createStudent(student);
         return Response.ok(student).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents() {
+        List<Student> allStudents = studentService.getAllStudents();
+        return Response.ok(allStudents).build();
     }
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -5,6 +5,9 @@ import se.iths.entity.Student;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Transactional
 public class StudentService {
@@ -15,5 +18,12 @@ public class StudentService {
     public Student createStudent(Student student) {
         em.persist(student);
         return student;
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> allStudents = em.createQuery("select s from Student s", Student.class).getResultList();
+        if (allStudents.isEmpty())
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        return allStudents;
     }
 }
